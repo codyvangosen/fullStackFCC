11505d9b5f96bdc47b412530c03ac270f6f5a0fc
Cody VanGosen <codyvangosen@gmail.com>
2025-06-05 22:57:00 -0500

    Initial commit of FreeCodeCamp full-stack project files.

---
+++ sentenceMaker/sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "big";
+let noun = "dog";
+let verb = "barking";
+let place = "home";
+let adjective2 = "wide";
+let noun2 = "cats";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "small";
+noun = "pig";
+verb = "oinking";
+place = "farm";
+adjective2 = "small";
+noun2 = "corn";
+
+let secondStory = ("Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".");
+
+console.log("Second story: " + secondStory);
---
+++ triviaBot/triviaBot.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Tom";
+let botLocation = "Pine St";
+let favoriteLanguage = "C++";
+
+let botDescription = "My name is " + botName + " and I live on " + botLocation + ".";
+
+console.log(botDescription);
+
+let botFavorite = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log(botFavorite);
+
+let codingFact = favoriteLanguage + " is a powerful high-level programming language.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " has many powerful pre-built libraries to use.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used in the Unity game engine.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
